7d51caf946249647e08cb440704f694dda60b0ed
Simeon Prause <simeon.prause@icloud.com>
2023-08-28 12:17:12 +0200

    2015/4/cl/2

--- 2015/4/cl/main.lisp
+++ 2015/4/cl/main.lisp
@@ -8,11 +8,13 @@
 (defun five-leading-zeros-p (s)
   (string= (subseq s 0 5)
 	   "00000"))
-
-(defun find-lowest-number (n)
-  (if (five-leading-zeros-p
+(defun n-leading-zeros-p (n s)
+  (string= (subseq s 0 n)
+	   (make-string n :initial-element #\0)))
+(defun find-lowest-number (n leading-zeros)
+  (if (n-leading-zeros-p leading-zeros
        (hex-vector-to-string
 	(md5:md5sum-string
 	 (concatenate 'string secret-key (format nil "~a" n)))))
       n
-      (find-lowest-number (1+ n))))
+      (find-lowest-number (1+ n) leading-zeros)))
