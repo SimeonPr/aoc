a06002f2b6ee68fb6bd8c3e05d5c1c19cc4a627e
Simeon Prause <simeon.prause@icloud.com>
2023-08-28 10:02:40 +0200

    Hard Challenge 3/12/2015

--- 2015/3/cl/main.lisp
+++ 2015/3/cl/main.lisp
@@ -6,15 +6,28 @@
 
 
 (let ((visited-houses (make-hash-table :test #'equalp))
-      (current-location (cons 0 0))
-      (house-counter 1))
+      (current-location-santa (cons 0 0))
+      (current-location-robo (cons 0 0))
+      (house-counter 1)
+      (santas-turn t))
   (setf (gethash current-location visited-houses) t)
   (with-open-file (in "../input.txt")
     (let ((instructions (read-line in nil nil)))
       (map nil #'(lambda (c)
-		   (setf current-location (update-location c current-location))
-		   (unless (gethash current-location visited-houses)
-		     (incf house-counter)
-		     (setf (gethash current-location visited-houses) t)))
+		   (if santas-turn
+		       (progn
+			 (setf current-location-santa
+			       (update-location c current-location-santa))
+			 (unless (gethash current-location-santa visited-houses)
+			   (incf house-counter)
+			   (setf (gethash current-location-santa visited-houses) t))
+			 (setf santas-turn (not santas-turn)))
+		       (progn
+			 (setf current-location-robo
+			       (update-location c current-location-robo))
+			 (unless (gethash current-location-robo visited-houses)
+			   (incf house-counter)
+			   (setf (gethash current-location-robo visited-houses) t))
+			 (setf santas-turn (not santas-turn)))))
 	   instructions)
       (format t "Unique houses: ~a~%" house-counter))))
